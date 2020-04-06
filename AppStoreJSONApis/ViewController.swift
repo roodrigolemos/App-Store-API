391c9a8cd414aebb4956d058a071f2b0b70a5299
RodrigoLemos <rodlemoss@gmail.com>
2020-04-06 18:53:59 -0400

    Initial Commit

---
+++ AppStoreJSONApis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApis
+//
+//  Created by Rodrigo Lemos on 06/04/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
