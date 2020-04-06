91d360856b18dfaea6c120c7fed476a13966cd7d
RodrigoLemos <rodlemoss@gmail.com>
2020-04-06 19:57:56 -0400

    Setting up the foundation with UITabBarController

---
+++ AppStoreJSONApis/BaseTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreJSONApis
+//
+//  Created by Rodrigo Lemos on 06/04/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
+        
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = UIImage(named: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.navigationBar.prefersLargeTitles = true
+        blueNavController.tabBarItem.image = UIImage(named: "search")
+        
+        viewControllers = [redNavController, blueNavController]
+    }
+}
