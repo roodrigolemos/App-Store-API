4cd50c60c0c6b52a668c5729ff62003d8b125b86
RodrigoLemos <rodlemoss@gmail.com>
2020-04-08 13:15:38 -0400

    iTunes Search API JSON Fetch

--- AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -17,6 +17,28 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         
         collectionView.backgroundColor = .white
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+        
+        fetchItunesApps()
+    }
+    
+    fileprivate func fetchItunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            if let err = err {
+                print("Failed to fetch apps: ", err)
+                return
+            }
+            guard let data = data else { return }
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+            } catch let jsonErr {
+                print("Failed to decode json:", jsonErr)
+            }
+        }.resume()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -28,7 +50,8 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = "Here is my app name"
         return cell
     }
     
---
+++ AppStoreJSONApis/Model/SearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResult.swift
+//  AppStoreJSONApis
+//
+//  Created by Rodrigo Lemos on 08/04/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+   
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
