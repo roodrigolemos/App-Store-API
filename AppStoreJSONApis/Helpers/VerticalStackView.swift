39cb72479703824f3a3f31bc139891c27bd509ff
RodrigoLemos <rodlemoss@gmail.com>
2020-04-08 10:44:56 -0400

    Extensions and Helpers for Code Reduction

---
+++ AppStoreJSONApis/Helpers/VerticalStackView.swift
@@ -0,0 +1,9 @@
+//
+//  VerticalStackView.swift
+//  AppStoreJSONApis
+//
+//  Created by Rodrigo Lemos on 08/04/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import Foundation
