4d4958ee2f9226fc3bf4722d310d86fa7b901665
RodrigoLemos <rodlemoss@gmail.com>
2020-04-09 23:22:44 -0400

    App Row Cells

---
+++ AppStoreJSONApis/Helpers/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  AppStoreJSONApis
+//
+//  Created by Rodrigo Lemos on 09/04/20.
+//  Copyright © 2020 Rodrigo Lemos. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
+
