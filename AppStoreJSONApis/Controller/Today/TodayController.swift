7ba64ef151ac51ed8d79ad5da27ef0f749e2a4c0
RodrigoLemos <rodlemoss@gmail.com>
2020-05-04 20:20:14 -0400

    Card Dismissal Fixes

--- AppStoreJSONApis/Controller/Today/TodayController.swift
+++ AppStoreJSONApis/Controller/Today/TodayController.swift
@@ -125,19 +125,36 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout, U
         return true
     }
     
+    var appFullscreenBeginOffset: CGFloat = 0
+    
     @objc fileprivate func handleDrag(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .began {
+            appFullscreenBeginOffset = appFullscreenController.tableView.contentOffset.y
+        }
+        
+        if appFullscreenController.tableView.contentOffset.y > 0 {
+            return
+        }
+        
         let translationY = gesture.translation(in: appFullscreenController.view).y
         
         if gesture.state == .changed {
-             let scale = 1 - translationY / 1000
-                   
-            let transform: CGAffineTransform = .init(scaleX: scale, y: scale)
-            self.appFullscreenController.view.transform = transform
+            if translationY > 0 {
+                let trueOffset = translationY - appFullscreenBeginOffset
+                
+                var scale = 1 - trueOffset / 1000
+                
+                scale = min(1, scale)
+                scale = max(0.5, scale)
+                       
+                let transform: CGAffineTransform = .init(scaleX: scale, y: scale)
+                self.appFullscreenController.view.transform = transform
+            }
         } else if gesture.state == .ended {
-            handleAppFullscreenDismissal()
+            if translationY > 0 {
+                handleAppFullscreenDismissal()
+            }
         }
-        
-        
     }
     
     fileprivate func setupStartingCellFrame(_ indexPath: IndexPath) {
@@ -224,6 +241,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout, U
             self.tabBarController?.tabBar.frame.origin.y = self.view.frame.height - 80
             
             guard let cell = self.appFullscreenController.tableView.cellForRow(at: [0,0]) as? AppFullscreenHeaderCell else { return }
+            cell.closeButton.alpha = 0
             cell.todayCell.topConstraint.constant = 24
             cell.layoutIfNeeded()
             
